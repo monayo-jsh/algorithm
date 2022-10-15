3bb4a77a7b6c62561245cd8f425887825fe75e46
주상현 <monayo.jsh@gmail.com>
2022-10-16 01:12:36 +0900

    template용

---
+++ src/main/java/codility/Template.java
@@ -0,0 +1,11 @@
+package codility;
+
+public class Template {
+    public static void main(String[] args) {
+        System.out.println(solution("param"));
+    }
+
+    public static String solution(String param) {
+        return null;
+    }
+}
